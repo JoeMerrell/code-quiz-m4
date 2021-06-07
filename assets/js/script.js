2e817b828acd5a97cb722bc98844194fb25090e6
Joseph <joe@uhhuhohyeah.com>
2021-06-06 17:32:09 -0700

    added separate js file for the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//selecting all required elements
+// QUERY SELECTORS
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
@@ -9,6 +9,8 @@ const option_list = document.querySelector(".option_list");
 const time_line = document.querySelector("header .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
+
+
 // if startQuiz button clicked
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show info box
